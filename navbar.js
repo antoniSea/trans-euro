0b062c0b3644a4a6dfc18e5a2fcb5dee9c58ebde
Antoni Seba <antoniwoj@o2.pl>
2023-01-18 14:52:56 +0100

    Change navbar behaviour and font size of first section text

--- navbar.js
+++ navbar.js
@@ -14,6 +14,24 @@ document.getElementById('mobile-toggle-button').addEventListener('click', (() =>
     }
 }));
 
+// get every a element in the navbar
+const navLinks = document.querySelectorAll('#mobile-navbar-open a');
+navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+        const nav = document.getElementById("mobile-navbar-open");
+    
+        if (nav.style.display === "flex") {
+            nav.classList.add("mobile-navbar-open-out");
+            setTimeout(() => {
+                changeDisplay(nav, "none");
+                nav.classList.remove("mobile-navbar-open-out");
+            }, 500)
+        } else {
+            changeDisplay(nav, "flex");
+        }
+    })
+})
+
 const changeDisplay = (element, display) => {
     element.style.display = display;
 }
